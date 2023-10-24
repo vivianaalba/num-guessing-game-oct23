7271dc34a3eff4ad95c7dd211b966c875cce1257
Viviana Alba <vivianaalba7@gmail.com>
2023-10-24 07:55:04 -0700

    edit bugs on while loop

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ function checkGuess() {
   let guess = Number(document.getElementById("guess").value);
   let numGuess = parseInt(guess);
 
-  while (numGuess !== NaN) {
-    if (numGuess < 1 || numGuess > 100) {
-        prompt("Number is not in range, try again.");
-    } else if (numGuess !== number) {
-        prompt("Wrong number, try again.")
-    } else if (number === numGuess) {
-        prompt("You got it!")
-    }
-}
-
-
+  while (numGuess !== number) {
+    if (numGuess < 0 || numGuess > 100) {
+        resultSpan.innerText = "Number not in range. Try again.";
+        break;
+        } else if (numGuess !== number) {
+        resultSpan.innerText = "Wrong number. Try again.";
+        break;
+        }
+    };  
+    if (number === numGuess) {
+        resultSpan.innerText = "You guessed correctly!";    
+    };
 };
 
 button.addEventListener("click", checkGuess);
